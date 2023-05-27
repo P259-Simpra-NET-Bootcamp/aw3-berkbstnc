fccbeac8eeba13af5d7afdd0c4d9aae2de4b68e5
Ali Berk Bostancı <55848455+berkbstnc@users.noreply.github.com>
2023-05-27 23:04:18 +0300

    ödev yüklendi

---
+++ SimApi/SimApi.Schema/Mapper/MapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SimApi.Data;
+
+namespace SimApi.Schema;
+
+public class MapperProfile : Profile 
+{
+    public MapperProfile()
+    {
+        CreateMap<Category, CategoryResponse>();
+        CreateMap<CategoryRequest, Category>();
+
+        CreateMap<Product, ProductResponse>();
+        CreateMap<ProductRequest, Product>();
+    }
+
+
+}
